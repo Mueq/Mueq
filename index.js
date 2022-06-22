4d6707658f53c6ad27408525efd9c8cf800c617c
Mueq <107995021+Mueq@users.noreply.github.com>
2022-06-22 17:54:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const chalk = require('chalk');
+const rs = require('readline-sync');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const GoStumble = (auth) => new Promise((resolve, reject) => {
+
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+    method: 'GET',
+    headers: {
+      'authorization': auth
+    }
+  })
+    .then(res => res.text())
+    .then(data => {
+      resolve(data);
+    })
+    .catch(err => {
+      reject(err);
+    });
+
+});
+
+(async () => {
+
+  console.log(`Trophy And Crown Hack Safe! [ReEdit]
+By : ${chalk.white('salamuu#8745')} - Credit : @dkmpostor & @Eskey
+`);
+
+  const auth = rs.question('Masukan mode Authen! : ');
+  console.log('');
+
+  while (true) {
+
+
+    const result = await GoStumble(auth);
+    if (!result) {
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+      const crown = data.User.Crowns;
+
+console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.gold(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
+      await sleep(6000);
+
+    } else if (result == 'BANNED') {
+      console.log(chalk.bgRed(`Your Account Anda Telah Di Banned`));
+     break;
+    }
+  }
+
+
+})();
